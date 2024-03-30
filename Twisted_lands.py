2b63e20a80f35df4b6d4c06ec67c58319f49ad08
mnaselli <matiasnaselli@gmail.com>
2024-03-30 16:30:57 -0300

    remade slide logic

--- Twisted_lands.py
+++ Twisted_lands.py
@@ -14,7 +14,7 @@ pygame.font.init()
 # CLASSES
 # =============================================================================
 class Slide:
-    def __init__(self,slide_id, text_template, category,city_advancement = None,button_text = None, spell = None,item = None, previous_slide = None, linked_slide_id=None, is_fight=False,background =None):
+    def __init__(self,slide_id, text_template, category,test = None,city_advancement = None,button_text = None, spell = None,item = None, previous_slide = None, success_slide_id = None, fail_slide_id = None, linked_slide_id=None, is_fight=False,background =None):
         self.slide_id = slide_id
         self.text = text_template
         self.text_template = text_template
@@ -27,6 +27,15 @@ class Slide:
         self.use_spell = spell
         self.use_item = item
         self.button_text = button_text if button_text is not None else ["move forward"]
+        self.test = test
+        self.success_slide_id = success_slide_id
+        self.fail_slide_id = fail_slide_id
+
+    def is_terminal(self):
+        return not self.linked_slide_id and not self.success_slide_id
+
+    
+
 
 class Character:
     def __init__(self,name,job,strenght,agility,lore,faith):
@@ -60,22 +69,23 @@ slides = {
         #Slide("wild2","A clearing in the forest reveals a peaceful river.","wild"),
         Slide("wild3","The path leads you to the edge of a cliff with a breathtaking view.","wild"),
         Slide("wild4","A clearing in the forest reveals a giant tree.","wild", linked_slide_id=["chainGiantTree1","chainGiantTree2","chainGiantTree3"]),
-        Slide("wild5","You enter the grove. [/color] flowers surround you filling the air with a [/smellqual] aroma of [/smell]. The birds sing, filling you with [/emotion].","wild")
+        Slide("wild5","You enter the grove. [/color] flowers surround you filling the air with a [/smellqual] aroma of [/smell]. The birds sing, filling you with [/emotion].","wild"),
+        Slide("wild6","You are crossing a bridge and it starts to break","wild", success_slide_id=["bridge_success"],fail_slide_id=["bridge_fail1,bridge_fail2"],button_text = ["run fast to the end","take a big jump"],test = ["agility","strength"]),
     ],
     "city": [
         Slide("city1","You arrive to the city of ......","city", background="city"),
         Slide("city2","You stand before the doors of ......","city", background="city"),
         Slide("city3","At the distance you see the great city of .........","city", background="city")
     ],
-    "encounter": [
-        Slide("encounter1","A strange animal approaches you menacingly.","encounter"),
-        Slide("encounter2","A human like creature ambushes you, weapons at hand.","encounter"),
-        Slide("encounter3","An old friend finds you, bringing news from home.","encounter")
-    ],
     "chain": [
         Slide("chainGiantTree1","You approach the giant tree and as you blink it dissapears","chain",background = "woodbackground"),
         Slide("chainGiantTree2","You approach the giant tree and as you blink it dissapears22","chain",background = "woodbackground"),
         Slide("chainGiantTree3","You go around the giant tree, and the trunk seems endless","chain",background = "woodbackground")
+    ],
+    "test": [
+        Slide("bridge_success","You approach the giant tree and as you blink it dissapears","ctest"),
+        Slide("bridge_fail1","You are not fast enough and fall taking X damage","test"),
+        Slide("bridge_fail2","Your jump is not long enough and you fall taking X damage","test"),
     ],
     "character": [
         Slide("character_info","","character")
@@ -84,10 +94,13 @@ slides = {
 
 
 text_options = {
-    "[/color]": ["Purple", "Red", "Yellow", "Vibrant", "Pale"],
-    "[/smellqual]": ["sweet", "strong", "delicate", "overpowering", "unpleasant"],
-    "[/smell]": ["roses", "lavender", "corpses", "fresh rain", "pine"],
+    "[/color]": ["purple", "red", "yellow", "green", "black", "white", "brown", "orange", "grey", "indigo", "maroon", "golden", "silver", "pink", "azure", "violet", "translucent", "blue", "scarlet", "vermillion", "cherry", "cerulean", "viridescent", "lime", "amber", "beige", "bordeaux", "vinaceus", "burgundy", "colourless", "chalky", "dark", "inky", "ochre", "iridescent", "opalescent", "prismatic"],
+    "[/colorqual]": ["pale", "strong", "faint", "spotty", "uniform", "deep", "faded", "shimmering", "oily", "intense", "reflective", "metallic", "shiny", "glossy", "fluorescent", "opaque", "glowing", "vivid"],
+    "[/smell]": ["roses", "lavender", "powder", "blood", "metal", "corpses", "rot", "mold", "dirt", "excrement", "ozone", "chemicals", "brine", "fish", "fruit", "carrion", "burning", "sweat", "petrichor"],
+    "[/emotionpositive]": ["happiness", "tranquility", "valor", "bravery", "hope", "energy", "satisfaction", "cheerfulness", "courage"],
+    "[/emotionnegative]": ["sadness", "fear", "foreboding", "dread", "hopelessness", "fatigue", "despair", "angst", "anger"],
     "[/emotion]": ["dread", "joy", "peace", "excitement", "anxiety"],
+    "[/smellqual]": ["faint", "strong", "weak", "barely perceptible", "pungent", "invasive", "notorious"],
 }
 
 
@@ -124,12 +137,6 @@ button_city = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((550, 550),
                                            text='Visit city',
                                            manager=ui_manager)
 
-# =============================================================================
-# button_encounter = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((550, 550), (200, 50)),
-#                                                  text='Encounter',
-#                                                  manager=ui_manager)
-# =============================================================================
-
 character_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((710, 10), (50, 30)),
                                                 text='C',
                                                 manager=ui_manager)
@@ -151,6 +158,11 @@ background_image = loaded_backgrounds.get(current_slide.background, loaded_backg
 button_paths = []
 button_paths_ids = []  # This will store the next slide IDs corresponding to each button in button_paths
 
+
+button_paths_linked = []
+button_paths_ids_linked = []
+button_paths_test = []
+button_paths_ids_test = []
 
 testy = Character("Testy","Test character",1,1,1,1)
 
@@ -194,10 +206,12 @@ def display_character_info(character):
     # This is a simplified example. Adjust according to your UI design.
     
     button_wild.hide()
-    #button_city.hide()
+    button_city.hide()
     #button_encounter.hide()
-    for btn in button_paths:
+    for btn in button_paths_linked + button_paths_test:
         btn.hide()
+    
+    
     
     with open("boxes_config.json", "r") as file:
         boxes_config = json.load(file)["boxes"]
@@ -208,6 +222,14 @@ def display_character_info(character):
     for text, box in zip(texts, boxes_config):
         draw_box_with_border(window, text, box)
     
+def agility_test(character):
+    # Placeholder logic for agility test
+    return random.choice([True, False])
+
+def strength_test(character):
+    # Placeholder logic for strength test
+    return random.choice([True, False])
+
 
 # =============================================================================
 # # =============================================================================
@@ -279,28 +301,6 @@ def on_category_button_click(category):
     current_category = category
     select_random_slide(category)
 
-
-
-
-def update_path_buttons(slide, ui_manager):
-    # Clear existing buttons
-    for btn in button_paths:
-        btn.kill()
-    button_paths.clear()
-    button_paths_ids.clear()
-
-    # Assuming slide.linked_slide_id is a list of slide IDs for next possible slides
-    for index, slide_id in enumerate(slide.linked_slide_id):
-
-        # Modified positioning to include a gap between buttons
-        btn = pygame_gui.elements.UIButton(
-            relative_rect=pygame.Rect((100 + index * (210 + 10), 500), (200, 50)),  # Added a 10-pixel gap# Adjusted for a 20px gap
-            text=f"Option {index + 1}",
-            manager=ui_manager
-        )
-        button_paths.append(btn)
-        button_paths_ids.append(slide_id)
-
 def on_character_button_click():
     global current_slide
     if current_slide.slide_id != "character_info":
@@ -311,10 +311,58 @@ def on_character_button_click():
     elif current_slide.previous_slide is not None:
         # Go back to the previous slide
         current_slide = getSlidebyID(current_slide.previous_slide)
-        if button_paths:
-            for btn in button_paths:
+        if button_paths_linked or button_paths_test :
+            for btn in button_paths_linked + button_paths_test:
                 btn.show()
 
+
+def update_path_buttons(slide, ui_manager):
+    
+    global button_paths_linked, button_paths_ids_linked,button_paths_linked_test, button_paths_ids_test
+    if slide.is_terminal():
+        button_wild.show()
+    else:
+        button_wild.hide()
+    
+    for btn in button_paths_linked + button_paths_test:
+        btn.kill()
+    button_paths_linked.clear()
+    button_paths_ids_linked.clear()
+    button_paths_test.clear()
+    button_paths_ids_test.clear()
+# =============================================================================
+#   linked slide
+# =============================================================================
+    
+    if not slide.is_terminal() and slide.linked_slide_id:
+    # Assuming slide.linked_slide_id is a list of slide IDs for next possible slides
+        for index, slide_id in enumerate(slide.linked_slide_id):
+            button_text = slide.button_text[index] if index < len(slide.button_text) else "Option"
+            # Modified positioning to include a gap between buttons
+            btn = pygame_gui.elements.UIButton(
+                relative_rect=pygame.Rect((100 + index * (210 + 10), 500), (200, 50)),  # Added a 10-pixel gap# Adjusted for a 20px gap
+                text=button_text,
+                manager=ui_manager
+            )
+            button_paths_linked.append(btn)
+            button_paths_ids_linked.append(slide_id)
+
+# =============================================================================
+#   test slides
+# =============================================================================
+    
+    test_functions = [agility_test, strength_test]  # List of test functions
+    if slide.test:
+        # Handle test slide button creation with specific outcomes
+        for index, test_action in enumerate(slide.button_text):
+            btn = pygame_gui.elements.UIButton(
+                relative_rect=pygame.Rect((100, 100 + index * 60), (200, 50)),
+                text=test_action,
+                manager=ui_manager
+            )
+            button_paths_test.append(btn)
+            # Store the test function along with the index
+            button_paths_ids_test.append((test_functions[index], index))
 # =============================================================================
 # LOOP
 # =============================================================================
@@ -333,47 +381,66 @@ while running:
         ui_manager.process_events(event)
             
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            if event.ui_element ==button_wild:
-                # Handles category button clicks
-                category = 'wild'
+            if event.ui_element == button_wild:
+                select_random_slide("wild")  # Set the current slide to a random wild slide
+                update_buttons_for_slide(current_slide)  # Update UI buttons for the new slide
+                update_path_buttons(current_slide, ui_manager) 
+            
+            
+            if event.ui_element in button_paths_linked:
+                index = button_paths_linked.index(event.ui_element)
+                linked_slide_id = button_paths_ids_linked[index]
+                current_slide = getSlidebyID(linked_slide_id)
+                update_path_buttons(current_slide, ui_manager)  # Update buttons for the new slide
 
-                on_category_button_click(category)  # Function to handle category selection
-
-                #current_slide_text = generate_dynamic_text(current_slide.text_template, text_options)
-                update_buttons_for_slide(current_slide)  # Update buttons based on the new slide
-                update_path_buttons(current_slide, ui_manager)
-
-            elif event.ui_element in button_paths:
-                # Handles clicks on dynamically created path buttons
-                index = button_paths.index(event.ui_element)  # Find the pressed button
-                slide_id = button_paths_ids[index]  # Get the associated slide ID
-                next_slide = get_linked_slide(slide_id)  # Retrieve the slide object
-                if next_slide:
-                    current_slide = next_slide  # Transition to the next slide
-                    current_slide_text = generate_dynamic_text(current_slide.text_template, text_options)  # Update text
-                    if current_slide.linked_slide_id:
-                        update_path_buttons(current_slide, ui_manager)  # Update buttons for the next slide
+            
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element in button_paths_test:
+                    index = button_paths_test.index(event.ui_element)
+                    test_function, action_index = button_paths_ids_test[index]
+            
+                    # Call the test function
+                    test_passed = test_function(testy)  # Assuming `testy` is your character instance
+            
+                    if test_passed:
+                        next_slide_id = current_slide.success_slide_id[0]  # Navigate to success slide
                     else:
-                        # Logic to handle the end of a chain
-                        button_wild.show()
-                        #button_city.show()
-                        #button_encounter.show()
-                        for btn in button_paths:  # Remove path buttons
-                            btn.kill()
-                        button_paths.clear()
-                        button_paths_ids.clear()
+                        next_slide_id = current_slide.fail_slide_id[action_index]  # Navigate to specific fail slide
+            
+                    current_slide = getSlidebyID(next_slide_id)
+                    
+                    update_path_buttons(current_slide, ui_manager)
+            
 # =============================================================================
-#             if event.type == pygame_gui.UI_BUTTON_PRESSED:
-#                 if event.ui_element in button_paths:
-# =============================================================================
-                if event.ui_element in button_paths:                
-                    index = button_paths.index(event.ui_element)
-                    # Handle the button click as before
-                update_path_buttons(current_slide, ui_manager)
-# =============================================================================
-#             if event.type == pygame_gui.UI_BUTTON_PRESSED:
-#                 if current_slide.linked_slide_id:
-#                     update_path_buttons(current_slide, ui_manager)            
+#             if event.ui_element ==button_wild:
+#                 # Handles category button clicks
+#                 category = 'wild'
+# 
+#                 on_category_button_click(category)  # Function to handle category selection
+# 
+#                 #current_slide_text = generate_dynamic_text(current_slide.text_template, text_options)
+#                 update_buttons_for_slide(current_slide)  # Update buttons based on the new slide
+#                 update_path_buttons(current_slide, ui_manager)
+# 
+#             elif event.ui_element in button_paths:
+#                 # Handles clicks on dynamically created path buttons
+#                 index = button_paths.index(event.ui_element)  # Find the pressed button
+#                 slide_id = button_paths_ids[index]  # Get the associated slide ID
+#                 next_slide = get_linked_slide(slide_id)  # Retrieve the slide object
+#                 if next_slide:
+#                     current_slide = next_slide  # Transition to the next slide
+#                     current_slide_text = generate_dynamic_text(current_slide.text_template, text_options)  # Update text
+#                     if current_slide.linked_slide_id:
+#                         update_path_buttons(current_slide, ui_manager)  # Update buttons for the next slide
+#                     else:
+#                         # Logic to handle the end of a chain
+#                         button_wild.show()
+#                         #button_city.show()
+#                         #button_encounter.show()
+#                         for btn in button_paths:  # Remove path buttons
+#                             btn.kill()
+#                         button_paths.clear()
+#                         button_paths_ids.clear()
 # =============================================================================
             
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
@@ -382,7 +449,7 @@ while running:
             
            
             current_slide_text = generate_dynamic_text(current_slide.text_template, text_options)
-            update_buttons_for_slide(current_slide)  # Update button visibility based on the new current slide.
+            #update_buttons_for_slide(current_slide)  # Update button visibility based on the new current slide.
          
         ui_manager.process_events(event)
 
